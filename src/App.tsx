0bc8d18cfe0f2573ef59f1213d4e910f8bee1e35
Pedro Dal Bianco <dalbianco.pedro@gmail.com>
2023-01-19 16:56:20 -0300

    Add router and update eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react"
+import {
+	createBrowserRouter,
+	RouterProvider,
+} from "react-router-dom"
 
-function App (): JSX.Element {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <div>Hello world!</div>,
+	},
+	{
+		path: "/1",
+		element: <div>Hello world 1!</div>,
+	},
+])
+
+function App() {
+	return (
+		<RouterProvider router={router}/>
+	)
 }
 
 export default App
